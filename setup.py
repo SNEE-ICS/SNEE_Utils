06adf619b2fa3ad907afcca39f9eceedcf7ed2a7
Ibrahim Khan <148237791+IbrahimKhan07@users.noreply.github.com>
2025-07-21 15:39:04 +0000

    Changes

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="python_utils",
     version="0.1.0",
-    packages=find_packages(),
+    packages=find_packages(),  # This will find both py_utils and style_utils
     description="Common Python utility functions for reuse across projects.",
     author="Ibrahim Khan",
-    license="MIT",
-    install_requires=[],
+    license="Apache-2.0",
+    license_files=["LICENSE"],
+    install_requires=[],  # Add any dependencies here or parse requirements.txt if needed
+    include_package_data=True,  # <- This allows MANIFEST.in to include non-code files
+    package_data={
+        "snee_styles": ["snee.mplstyle","SNEE_plotly.json"],  # Explicitly include the .mplstyle file
+    },
 )
